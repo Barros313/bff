26dd2e965e3a39734341b394c8f2a3a1d9388da4
Gabriel Barros de Almeida <g.barros.de.almeida@avanade.com>
2025-03-20 18:52:59 -0300

    Removed TestTest class
    Signed-off-by: Gabriel Barros de Almeida <g.barros.de.almeida@avanade.com>

--- src/main/java/com/avanade/bff/domain/model/TestTest.java
+++
@@ -1,8 +0,0 @@
-package com.avanade.bff.domain.model;
-
-public class TestTest {
-    public static void main(String[] args) {
-        Customer customer = new Customer();
-        System.out.println(customer);
-    }
-}
